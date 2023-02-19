e30c94462a765b31f3138d2ab6b0c723c05a780f
Hott-J <jhj13062004@naver.com>
2023-02-19 23:28:30 +0900

    clear git cache

---
+++ src/main/java/co/kr/tmax/openbanking_monolithic_be/OpenbankingMonolithicBeApplication.java
@@ -0,0 +1,13 @@
+package co.kr.tmax.openbanking_monolithic_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenbankingMonolithicBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenbankingMonolithicBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/kr/tmax/openbanking_monolithic_be/OpenbankingMonolithicBeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.tmax.openbanking_monolithic_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenbankingMonolithicBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
